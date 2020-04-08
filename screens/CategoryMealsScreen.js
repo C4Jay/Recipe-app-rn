3ddf70ab227f3bc937f0be93e8ba531fbd9127af
C4Jay <choolakejay@gmail.com>
2020-04-08 21:04:48 +0530

    route-work

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button} from 'react-native';
+import { CATEGORIES } from '../data/dummy';
 
 const CategoryMeals = (props) => {
+    const id = props.navigation.getParam('categoryId')
+    const category = CATEGORIES.find(cat => cat.id === id)
     return (
         <View style={styles.screen}>
-            <Text>CategoryMeals Screen</Text>
+            <Text>CategoryMeals</Text>
+            <Text>{category.title} </Text>
             <Button title="Go meals" onPress={() => props.navigation.navigate('MealDetail')}></Button>
         </View>
     )
