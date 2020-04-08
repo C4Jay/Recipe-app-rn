212ba6238e7ea1fb094f1b332608b15e47028f18
C4Jay <choolakejay@gmail.com>
2020-04-08 12:50:11 +0530

    route-work

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button} from 'react-native';
 
 const CategoryMeals = (props) => {
     return (
         <View style={styles.screen}>
             <Text>CategoryMeals Screen</Text>
+            <Button title="Go meals" onPress={() => props.navigation.navigate('MealDetail')}></Button>
         </View>
     )
 }
