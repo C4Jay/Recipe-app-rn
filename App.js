807213d22b063580a452d77d48db4f61f38c4d61
C4Jay <choolakejay@gmail.com>
2020-04-08 12:36:49 +0530

    npm-work

---
+++ App.js
@@ -0,0 +1,36 @@
+import React,{ useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+import Mealsnav from './nav/Mealsnav';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Regular.ttf')
+  })
+}
+export default function App() {
+
+  const [fontLoaded, setfontLoaded] = useState(false)
+
+  if(!fontLoaded) {
+    return (
+    <AppLoading startAsync={fetchFonts}
+    onFinish={() => setfontLoaded(true)} />
+    )
+  }
+  return (
+    <Mealsnav></Mealsnav>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
